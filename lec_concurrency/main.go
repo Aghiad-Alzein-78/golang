fab554daecaa619d64332f8543fa9603affb3bf5
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2021-08-01 01:57:05 +0300

    ----

--- lec_concurrency/main.go
+++ lec_concurrency/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+}
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Foo:", i)
+		time.Sleep(time.Millisecond * 2)
+	}
+	wg.Done()
+}
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Bar:", i)
+		time.Sleep(time.Millisecond * 1)
+	}
+	wg.Done()
 }
